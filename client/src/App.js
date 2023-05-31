6a765e7d1de7ff60269ad4a6cecce8e1164f40cf
rocketsummer <rocketsummer@users.noreply.github.com>
2023-05-31 18:10:34 -0500

    add sign in

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useState, useEffect} from 'react';
+
 function App() {
+  const [userid, setUserId] = useState(null);
+  const [useridInput, setUserIdInput] = useState('');
+
+  useEffect(() => {
+
+  }, [userid])
+
+  if(userid === null) {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <p >
+          Sign into DappBack Streaks
+        </p>
+        <input value={useridInput} onChange={(e) => setUserIdInput(e.target.value)} placeholder="Enter user id"></input>
+        <br></br>
+        <button onClick={() => setUserId(useridInput)} disabled={useridInput.length <= 0}>Sign in</button>
+        </header>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p >
+          Click daily for rewards!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
